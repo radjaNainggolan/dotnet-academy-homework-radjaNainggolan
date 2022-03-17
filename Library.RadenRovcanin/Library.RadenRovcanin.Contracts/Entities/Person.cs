daa895faf0139844be812013c7e163a8c69a11ad
Raden Rovcanin <radenrovcanin12@gmial.com>
2022-03-17 23:07:42 +0100

    Added Person and Adress Entities

---
+++ Library.RadenRovcanin/Library.RadenRovcanin.Contracts/Entities/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.RadenRovcanin.Contracts.Entities
+{
+    internal class Person
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+
+        public Adress adress { get; set; }
+
+
+    }
+}
