fb450ff02e2f862c625f2dd7bd0a6436e92f2e94
Raden Rovcanin <radenrovcanin12@gmial.com>
2022-03-27 00:54:21 +0100

    IUnitOfWork is defined, UnitOfWork is created and implemented

---
+++ Library.RadenRovcanin/Library.RadneRovcanin.Data.Db/Repositories/UnitOfWork.cs
@@ -0,0 +1,25 @@
+using Library.RadenRovcanin.Contracts.Repositories;
+using Library.RadneRovcanin.Data.Db.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.RadenRovcanin.Data.Db.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IPersonRepository people;
+        private readonly DbContext _dbContext;
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public IPersonRepository People
+            => people ?? new PersonRepository(_dbContext);
+
+        public async Task SaveChangesAsync()
+        {
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
