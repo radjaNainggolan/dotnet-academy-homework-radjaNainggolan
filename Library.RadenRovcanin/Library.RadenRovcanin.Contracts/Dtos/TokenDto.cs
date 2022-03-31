65ec668b16c9658829ecae837bd0d5e87796d41e
Raden Rovcanin <radenrovcanin12@gmial.com>
2022-04-01 00:22:34 +0200

    TokenDto properties are defined

--- Library.RadenRovcanin/Library.RadenRovcanin.Contracts/Dtos/TokenDto.cs
+++ Library.RadenRovcanin/Library.RadenRovcanin.Contracts/Dtos/TokenDto.cs
@@ -2,5 +2,8 @@ namespace Library.RadenRovcanin.Contracts.Dtos
 {
     public class TokenDto
     {
+        public string Token { get; set; } = default!;
+
+        public DateTime ExpiresAt { get; set; } = default!;
     }
 }
