a2d5b88a9dadc7fe31308f5c9f7f0cef7ce1f1ee
Raden Rovcanin <radenrovcanin12@gmial.com>
2022-04-14 14:08:49 +0200

    Rente Book when repository returns data is fiexd

--- Library.RadenRovcanin/Library.RadenRovcanin.Services.Tests/LibraryServicesTests.cs
+++ Library.RadenRovcanin/Library.RadenRovcanin.Services.Tests/LibraryServicesTests.cs
@@ -55,7 +55,6 @@ namespace Library.RadenRovcanin.Services.Tests
             var actualResult = await _sut.GetAll();
 
             // assert
-
             actualResult.Should().BeEmpty();
         }
 
@@ -96,26 +95,25 @@ namespace Library.RadenRovcanin.Services.Tests
             action.Should().Throw<EntityNotFoundException>();
         }
 
-        //[Fact]
-        //public async Task ReturnBook_WhenRepositoryReturnsData_ThanMapToDto()
-        //{
-        //    // arrange
-        //    var book = BookMock.Build();
-        //    var bookId = book.Id;
-        //    var person = PersonMock.Build();
-        //    var personId = person.Id;
+        [Fact]
+        public async Task ReturnBook_WhenRepositoryReturnsData_ThanMapToDto()
+        {
+            // arrange
+            var book = BookMock.Build();
+            var bookId = book.Id;
+            var person = PersonMock.Build();
+            person.RentedBooks.Add(book);
+            var personId = person.Id;
 
-        //    //_unitOfWorkMock.Setup(m => m.Books.GetByIdAsync(bookId)).ReturnsAsync(book);
-        //    _unitOfWorkMock.Setup(m => m.People.GetByIdWithBooksAsync(personId)).ReturnsAsync(person);
-        //    await _sut.RentBook(personId, bookId);
-        //    // act
-        //    await _sut.ReturnBook(personId, bookId);
+            _unitOfWorkMock.Setup(m => m.Books.GetByIdAsync(bookId)).ReturnsAsync(book);
+            _unitOfWorkMock.Setup(m => m.People.GetByIdWithBooksAsync(personId)).ReturnsAsync(person);
+            // act
+            await _sut.ReturnBook(personId, bookId);
 
-        //    // assert
-        //    _unitOfWorkMock.Verify(m => m.People.GetByIdWithBooksAsync(personId), Times.Once);
-        //    _unitOfWorkMock.Verify(m => m.Books.GetByIdAsync(bookId), Times.Once);
-        //    _unitOfWorkMock.Verify(m => m.SaveChangesAsync(), Times.Once);
-        //}
+            // assert
+            _unitOfWorkMock.Verify(m => m.People.GetByIdWithBooksAsync(personId), Times.Once);
+            _unitOfWorkMock.Verify(m => m.SaveChangesAsync(), Times.Once);
+        }
 
         [Fact]
         public void ReturnBook_WhenRepositoryCanNotFindPerson_ThanThrowException()
